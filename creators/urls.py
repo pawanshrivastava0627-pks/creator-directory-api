80a8c224070d1fd2428043e4418d486e4a818b19
Pawan Kumar Shrivastava <pawan.shrivastava0627@gmail.com>
2026-07-09 00:29:06 +0530

    Implement secure creator listing and detail APIs with tenant isolation

--- creators/urls.py
+++ creators/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreatorListView
+from .views import CreatorListView, CreatorDetailView
 
 urlpatterns = [
     path("creators/", CreatorListView.as_view(), name="creator-list"),
+    path("creators/<str:pk>/", CreatorDetailView.as_view(), name="creator-detail"),
 ]
--- creators/views.py
+++ creators/views.py
@@ -1,11 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Creator
 from .serializers import CreatorSerializer
 
 
 class CreatorListView(APIView):
+    
 
     def get(self, request):
 
@@ -16,6 +18,24 @@ class CreatorListView(APIView):
         serializer = CreatorSerializer(
             creators,
             many=True,
+            context={"request": request}
+        )
+
+        return Response(serializer.data)
+    
+class CreatorDetailView(APIView):
+
+    def get(self, request, pk):
+
+        creator = get_object_or_404(
+            Creator.objects.filter(
+                agency_links__agency=request.user.agency
+            ).distinct(),
+            pk=pk
+        )
+
+        serializer = CreatorSerializer(
+            creator,
             context={"request": request}
         )
 
